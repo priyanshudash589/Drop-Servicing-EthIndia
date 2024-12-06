e92afa35de9a40186c8bb233f62236e1c035a8ca
Priyanshu Dash <pdash3774@gmail.com>
2024-12-06 17:50:53 +0530

    setup complete for the frontend

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Hero } from "./pages/Hero";
+import { People } from "./pages/People";
+import { Pin } from "./pages/Pin";
+
+export default function App() {
+  return(
+    <>
+    <div className="bg-black w-screen h-screen flex justify-center items-center">
+        {/* <People />
+        <Pin /> */}
+      {/* <Hero /> */}
+    </div>
+
+    
+    </>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  darkMode: ["class", "class"],
+  theme: {
+  	extend: {
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		colors: {}
+  	}
+  },
+  plugins: [addVariablesForColors, require("tailwindcss-animate")],
+};
+
+function addVariablesForColors({ addBase, theme }) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+ 
+  addBase({
+    ":root": newVars,
+  });
+}
